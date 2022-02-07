b48a6a9fdafec568163f15090a6b9baeddc75da4
Olya Konstantin <olyakonstantin@ya.ru>
2022-02-07 18:57:05 +0300

    returned to working state...
    Signed-off-by: Olya Konstantin <olyakonstantin@ya.ru>

--- mySnake.js
+++ mySnake.js
@@ -1,34 +1,51 @@
+
 //в List помещается список всех ячеек из таблицы
-var List = document.querySelectorAll('td') 
+var List
 
-// n  - ранд знач от 0 до 399
-var n = getRandomInt(400) 
-
-List.item(n).classList.toggle('green') //toggle - вкл/выкл цвета ячейки
 var dir = -1 // направление
 var step = 19 // шаг
 
-var beep = setInterval(function(){ //функция работает с заданным интервалом (тут с 150мс)
-    List.item(n).classList.toggle('green')
-    if ((n + step * dir) > 399 ||(n + step * dir) < 0) { 
-        dir *= -1 
-        changeStep()  
-    }
+// n  - ранд знач от 0 до 399
+var n = Math.floor(Math.random() * 400);
 
-    if ((n+1) % 20 == 0 || n % 20 == 0) { 
-        changeStep()
-    }
-    n += (dir*step)
-    List.item(n).classList.toggle('green')
-}, 150)
-
-clearInterval(beep)
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
 
 var changeStep = function(){
-    if (step == 19) { step = 21 }
-    else { step = 19 }
+	step = step == 19 ?
+		21
+		: 19
 }
+
+
+
+var setup = function(){
+	List = document.querySelectorAll('td') 
+
+	//toggle - вкл/выкл цвета ячейки
+	List.item(n).classList.toggle('green') 
+
+	//функция работает с заданным интервалом (тут с 150мс)
+	var beep = setInterval(function(){ 
+		List.item(n).classList.toggle('green')
+
+		if((n + step * dir) > 399 
+				|| (n + step * dir) < 0){ 
+			dir *= -1 
+			changeStep()  
+		}
+
+		if((n+1) % 20 == 0 
+				|| n % 20 == 0){ 
+			changeStep()
+		}
+		n += dir * step
+
+		List.item(n).classList.toggle('green')
+	}, 150)
+
+	//clearInterval(beep)
+
+}
+
+
+
+
