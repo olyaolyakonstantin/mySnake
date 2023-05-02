24e6b08646c34851dd110f676416226ae0a14670
olya konstantin <olyakonstantin@ya.ru>
2023-05-02 20:08:24 +0800

    Olya playing
    Signed-off-by: olya konstantin <olyakonstantin@ya.ru>

--- mySnake.js
+++ mySnake.js
@@ -1,16 +1,4 @@
-
-var print123 = function(prefix){
-	prefix = prefix
-		?? '-> '
-	// this is one of two cases where we need ";" in JS:
-	// 	- leading ( .. ) / [ .. ] / { .. }
-	// 	- for( .. ; .. ; .. ){ .. }
-	;[1,2,3]
-		.map(function(n){
-			console.log(n) }) }
-
-
-var SPEED = 90
+var SPEED = 110
 
 // Keyboard control configuation
 //
@@ -42,45 +30,80 @@ var DIRECTIONS = {
 	down: 2,
 	right: 3,
 }
+
 var DIRECTIONS_IDX = Object.keys(DIRECTIONS)
 
+var snake = {
+  body: undefined ,
+  draw: function() {
+    for (var i in snake.body) {
+      field.cells.item(snake.body[i]).classList.add('green') }
+    field.cells.item(snake.body.pop()).classList.remove('green')
+  },
+  direction: ['up', 'down', 'left', 'right'][Math.floor(Math.random()*4)],
+}
 
+var field = {
+  cells: undefined,
+  height: undefined,
+  width: undefined,
+
+  cellCount: undefined,
+}
+
+var apple = {
+  position: undefined,
+
+  replace: function() {
+    field.cells.item(apple.position).classList.remove('orange')
+    apple.position = Math.floor(Math.random() * field.cellCount)
+    field.cells.item(apple.position).classList.add('orange')
+  }
+}
+
+var wall = {
+  cells: undefined,
+
+  draw: function() {
+    for (var i in wall.cells) {
+      field.cells.item(wall.cells[i]).classList.add('grey') }
+  }
+}
+
+
+var overflow = function(cells, d) {
+  // overflow: vertcal
+  if (cells[0] < 0
+      || cells[0] >= field.cellCount){
+    cells[0] += field.cellCount * -d
+  // overflow: horizontal
+  } else if((snake.direction == 'left'
+        && (cells[0]+1)%field.width == 0)
+      || (snake.direction == 'right'
+        && cells[0]%field.width == 0)){
+    cells[0] += field.width * -d
+  }
+}
 
 var setup = function() {
 
-	var LIST = document.querySelectorAll('td')
+	field.cells = document.querySelectorAll('td')
+  field.height = document.querySelectorAll('tr').length
+  field.cellCount = field.cells.length
+	field.width = field.cellCount / field.height
 
-	var height = document.querySelectorAll('tr').length
-	var cellCount = LIST.length
-	var width = cellCount / height
+	snake.body = [Math.floor(Math.random() * field.cellCount)]
+  //field.cells.item(snake.body).classList.add('green')
 
-	var snake = [Math.floor(Math.random() * cellCount)]
+	apple.position = Math.floor(Math.random() * field.cellCount)
+  field.cells.item(apple.position).classList.add('orange')
 
-	var apple = Math.floor(Math.random() * cellCount)
 	var score = 0
-
-	var gate1 = undefined
-	var gate2 = undefined
-
+	//var gate1 = undefined
+	//var gate2 = undefined
 	var step = 1
 
-
-	LIST.item(apple).classList.add('orange')
-	LIST.item(snake).classList.add('green')
-
-	var wall = [Math.floor(Math.random() * cellCount)]
-
-
-// Wall setup v.1
-/*
-	for (i=0; i<8; i++){
-		if ((wall[i] + width) < cellCount){
-				wall.push(wall[i] + width)
-		}
-	}
-*/
-
-	var direction = 'right'
+	wall.cells = [Math.floor(Math.random() * field.cellCount)]
 
 	var isPaused = false
 
@@ -92,79 +115,31 @@ var setup = function() {
 					true
 					: false
 			if(key in DIRECTIONS
-					&& (DIRECTIONS[direction] - DIRECTIONS[key])%2 != 0){
-				direction = key
+					&& (DIRECTIONS[snake.direction] - DIRECTIONS[key])%2 != 0){
+				snake.direction = key
 			}
 		})
-
 
 	var gameDraw = function(){
 		if (!isPaused) {
 
-			//* XXX Q: which implementation is better?
-			// XXX pros and cons:
-			// 		+ better editablity
-			// 			no logic du[plication
-			// 		- worse readabilit
-			// size of step...
-			var step = (direction == 'up'
-					|| direction == 'down') ?
-				width
+			// size of step
+			var step = (snake.direction == 'up'
+					|| snake.direction == 'down') ?
+				field.width
 				: 1
-			// direction of step...
-			var d = (direction == 'up'
-					|| direction == 'left') ?
+			// direction of step
+			var d = (snake.direction == 'up'
+					|| snake.direction == 'left') ?
 				-1
 				: 1
-			// move the snake...
-			snake.unshift(snake[0] + step*d)
-			// overflow: vertcal
-			if(snake[0] < 0
-					|| snake[0] >= cellCount){
-				snake[0] += cellCount * -d
-			// overflow: horizontal...
-			} else if((direction == 'left'
-						&& (snake[0]+1)%width == 0)
-					|| (direction == 'right'
-						&& snake[0]%width == 0)){
-				snake[0] += width * -d
-			}
 
+      snake.body.unshift(snake.body[0] + step*d)
+      overflow(snake.body, d)
 
-			/*/
-			// XXX pros and cons:
-			// 		+ better readability
-			// 			simpler structure
-			// 		- worse editability
-			// 			logic duplication in two differenet ways
-			switch (direction) {
-				case 'up':
-					snake.unshift(snake[0]-width)
-					if(snake[0] < 0){
-					  	snake[0] += cellCount
-					}
-					break
-				case 'down':
-					snake.unshift(snake[0]+width)
-					if(snake[0] >= cellCount){
-						snake[0] -= cellCount
-					}
-					break
-				case 'left':
-					snake.unshift(snake[0]-1)
-					if((snake[0]+1)%width == 0){
-						snake[0] += width
-					}
-					break
-				case 'right':
-					snake.unshift(snake[0]+1)
-					if(snake[0]%width == 0){
-						snake[0] -= width
-					}
-					break
-			}
-			//*/
-
+      //XXX
+      snake.draw()
+      wall.draw()
 
 			/*
 			// teleports the snake from gate to gate
@@ -174,92 +149,49 @@ var setup = function() {
 						: gate1
 					direction = DIRECTIONS_IDX[(DIRECTIONS[direction]+1) % 4]
 			}
-
 			// teleports the gates every 30 points
 			if (snake.length%3 == 0 && snake[0] == apple) {
 				LIST.item(gate1).classList.remove('grey')
 				LIST.item(gate2).classList.remove('grey')
-
-				gate1 = Math.floor(Math.random() * cellCount)
-				gate2 = Math.floor(Math.random() * cellCount)
-
+				gate1 = Math.floor(Math.random() * field.cellCount)
+				gate2 = Math.floor(Math.random() * field.cellCount)
 				LIST.item(gate1).classList.add('grey')
 				LIST.item(gate2).classList.add('grey')
 			}
 			*/
 
-			/*
-			// snake against the wall
-			for (var i in wall) {
-				if (snake[0] == wall[i]) {
-					clearInterval(tick)
-				}
-			}
-			*/
-
-			var tail = snake.pop()
-
-			//green color in snake
-			for(let i of snake){
-				LIST.item(i).classList.add('green')
-			}
-			LIST.item(tail).classList.remove('green')
-
-			// grey color in wall
-			for (var i in wall) {
-				LIST.item(wall[i]).classList.add('grey')
-			}
-
-
-			for(let i = 1; i < snake.length; i++) {
-				if (snake[0] == snake[i]) {
+      // gameover snake crash
+			for (var i = 1; i < snake.body.length; i++) {
+				if (snake.body[0] == snake.body[i]) {
 					clearInterval(tick)
 				}
 			}
 
-			for(var i in snake) {
-				if (snake[i] == apple) {
-					score += 10
-					if (snake[0] == apple) {
-						snake.push(tail)
 
-						// grow the wall
+      //snake eats APPLE
+    if (snake.body[0] == apple.position) {
+       score += 10
+       snake.body.push(snake.body.length + 1)
+       apple.replace()
 
+       //grow the WALL
+       if (field.cells.item(wall.cells[0] + step*d).classList.contains('grey')) {
+         wall.cells.unshift(Math.random() * field.cellCount)
+       } else {
+           wall.cells.unshift(wall.cells[0] + step*d)
+       }
+       overflow(wall.cells, d)
+   }
 
-						//	if
-						if (LIST.item(wall[0] + step*d).classList.contains('grey')) {
-							//LIST.item(0).classList.add('pink')
-							wall.unshift(Math.random() * cellCount)
-						} else {
-								wall.unshift(wall[0] + step*d)
-						}
-
-						// overflow: vertcal
-						if(wall[0] < 0
-								|| wall[0] >= cellCount){
-							wall[0] += cellCount * -d
-						// overflow: horizontal...
-						} else if((direction == 'left'
-									&& (wall[0]+1)%width == 0)
-								|| (direction == 'right'
-									&& wall[0]%width == 0)){
-							wall[0] += width * -d
-						}
-
-					}
-					LIST.item(apple).classList.toggle('orange')
-					apple = Math.floor(Math.random() * cellCount)
-
-					// if
-					if (wall.includes(apple)) {
-						apple = Math.floor(Math.random() * cellCount)
-					} else {
-							LIST.item(apple).classList.toggle('orange')
-					}
-
-				}
-			}
-
+   // doesn't work :\
+   //  MAKE AN IXCEPTION FOR APPLE IN THE WALL
+   /*
+   if (wall.includes(apple)) {
+     apple = Math.floor(Math.random() * field.cellCount)
+   } else {
+       LIST.item(apple).classList.toggle('orange')
+   }
+   */
 			document.getElementById('score').innerHTML = 'score: '+ score
 		}
   }
